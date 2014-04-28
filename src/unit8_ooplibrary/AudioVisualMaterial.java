46305928880119fe92fc521246e7d64603aed647
John Mambo <>
2014-04-27 23:44:44 -0500

    Added UNDEFINED to AudioVisualType enum Initialized local variables from Default constructor Added @override public String displayInfo() Added ImageIcon displayCover(URI file) Added Clip getSoundClip(URL fileURL) Added overloaded method playSoundClip(URL fileURL) Added overloaded method playSoundClip(final URL fileURL, long seconds)

--- src/unit8_ooplibrary/AudioVisualMaterial.java
+++ src/unit8_ooplibrary/AudioVisualMaterial.java
@@ -2,8 +2,25 @@
 
 package unit8_ooplibrary;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.ImageIcon;
 
 /**
@@ -13,6 +30,7 @@ import javax.swing.ImageIcon;
 public class AudioVisualMaterial extends LibraryMaterial{
    
     public enum AudioVisualType{
+        UNDEFINED(0),
         AUDIOTAPE(1),
         CD(2), 
         DVD(3);
@@ -38,6 +56,9 @@ public class AudioVisualMaterial extends LibraryMaterial{
     
     public AudioVisualMaterial() {
         super();
+        this.type = AudioVisualType.UNDEFINED;
+        this.coverImage = null;
+        this.soundClip = null;
     }
     
     public AudioVisualMaterial(AudioVisualType type, ImageIcon coverImage, Clip soundClip) {
@@ -82,5 +103,91 @@ public class AudioVisualMaterial extends LibraryMaterial{
     public void setCoverImage(ImageIcon coverImage) {
         super.setCoverImage(coverImage);
         this.coverImage = coverImage;
+    }
+    
+    @Override
+    public String displayInfo(){
+        StringBuilder str = new StringBuilder();
+        str.append(super.displayInfo());
+        str.append(String.format("Type: %s%n", this.getType()));
+        return str.toString();
+    }
+    public ImageIcon displayCover(URI file){
+        //TODO: Implement displayCover 
+        throw new UnsupportedOperationException();
+    } 
+    
+    public Clip getSoundClip(URL fileURL) throws IOException, LineUnavailableException, UnsupportedAudioFileException {
+        try {
+            Clip clip = AudioSystem.getClip();  
+            AudioInputStream inputStream;
+            // URI format: this.getClass().getResource("/Sound/sample.wav")
+            inputStream = AudioSystem.getAudioInputStream(fileURL); 
+            clip.open(inputStream);
+            while (!Thread.currentThread().isInterrupted()) {
+                //clip.start();
+                return clip;
+            }
+            clip.stop();
+        } catch (IOException | LineUnavailableException | UnsupportedAudioFileException ex) {
+          Logger.getLogger(Unit8_OOPLibrary.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    } 
+    
+    public void playSoundClip(URL fileURL) throws IOException, LineUnavailableException, UnsupportedAudioFileException {
+        try {
+            Clip clip = AudioSystem.getClip();  
+            AudioInputStream inputStream;
+            // URI format: this.getClass().getResource("/Sound/sample.wav")
+            inputStream = AudioSystem.getAudioInputStream(fileURL); 
+            clip.open(inputStream);
+            while (!Thread.currentThread().isInterrupted()) {
+                clip.start();
+            }
+            clip.stop();
+        } catch (IOException | LineUnavailableException | UnsupportedAudioFileException ex) {
+          System.out.println(ex);
+        }
+    }
+    
+    public void playSoundClip(final URL fileURL, long seconds) throws IOException, LineUnavailableException, UnsupportedAudioFileException {
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Clip clip = AudioSystem.getClip();                        
+                    AudioInputStream inputStream;
+                    // URI format: this.getClass().getResource("/Sound/sample.wav")
+                    inputStream = AudioSystem.getAudioInputStream(fileURL);                        
+                    clip.open(inputStream);
+                    while (!Thread.currentThread().isInterrupted()) {
+                        clip.start();
+                    }
+                    clip.stop();
+                } catch (IOException | LineUnavailableException | UnsupportedAudioFileException ex) {
+                    System.out.println(ex);
+                }
+            }
+        };
+
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        List<Callable<String>> callables = new ArrayList<>();
+
+        callables.add(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                runnable.run();
+                return "Play Sound";
+            }
+        });
+
+        try{
+            executor.invokeAny(callables, seconds, TimeUnit.SECONDS); // Run with a timeout of given Seconds.
+        }
+        catch(InterruptedException | ExecutionException | TimeoutException ex){
+            executor.shutdown();
+        }
+        executor.shutdown();
     }
 }
